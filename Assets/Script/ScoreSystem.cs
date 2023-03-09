d1203c93f58526a0276446654640fd7512342d3f
An Vo <107334075+truonganvo@users.noreply.github.com>
2023-03-09 20:24:55 +1100

    finalizing

--- Assets/Script/ScoreSystem.cs
+++ Assets/Script/ScoreSystem.cs
@@ -10,8 +10,7 @@ public class ScoreSystem : MonoBehaviour
 
     [SerializeField] MovingCube textScore;
 
-    [SerializeField] AudioSource hellYeah;
-    [SerializeField] AudioClip nice;
+    [SerializeField] GameObject audioManager;
 
     private int highScore = 0;
     private int score = 0;
@@ -21,18 +20,21 @@ public class ScoreSystem : MonoBehaviour
     {
         highScore = PlayerPrefs.GetInt("highscore", 0);
         highScoreText.text = "HighScores: " + highScore.ToString();
-        hellYeah.clip = nice;
     }
     private void Update()
     {
         scoreText.text = textScore.level.ToString() + " POINTS";
         score = textScore.level;
+        CheckingHighScore();
+    }
 
+    public void CheckingHighScore()
+    {
         if (highScore < score)
         {
-            hellYeah.Play();
             PlayerPrefs.SetInt("highscore", score);
             Debug.Log("Sound!!!!!!");
+            audioManager.SetActive(true);
         }
     }
 
